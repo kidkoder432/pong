b089a5de92fb27ae33cb67a7868e965984c50167
kidkoder432 <61898798+kidkoder432@users.noreply.github.com>
2021-01-05 21:05:51 -0800

    Added levels and intro screen (version 1.2)
    I added a level system, intro screen, and help screen. Also updated the AI.

--- pong/pong.py
+++ pong/pong.py
@@ -16,7 +16,10 @@ y = 250
 xa = 0; ya = 0
 z = 0
 pu, pd, cu, cd, cy = 0,0,0,0,0
-r = (rn.random() * 2) + 2
+levels = [x / 10 for x in range(2, 8)]
+ln= 0
+level = levels[ln]
+r = (rn.random() * level) + (level)
 a = (rn.random()) * 22 + 22 + (90 * m.floor(rn.random() * 4))
 intel = m.floor(rn.random() * 100)
 t = 400
@@ -28,15 +31,26 @@ p = pygame.draw.rect(scr, c, (5, py, 10, 140)) #Player's paddle
 b = pygame.draw.ellipse(scr, c, (x, y, 20, 20)) #Ball
 cp = pygame.draw.rect(scr, c, (485, cy, 10, 140)) #computer's paddle
 font = pygame.font.SysFont(None, 30)
-def message(t, x, y):
+pos = rn.randint(0, 140)
+def msg(t, x, y):
     text = font.render(t, True, (255,255,255), (0,0,0))
     textr = text.get_rect()
     textr.center = (x,y)
     scr.blit(text, textr)
 def scl(num, in_min, in_max, out_min, out_max):
     return (num - in_min) * (out_max - out_min) / (in_max - in_min) + out_min
+def message(text, px, py):
+    # get the height of the font
+    fontHeight = font.size("Tg")[1]
+    for l in text.split('\n'):
+        tr = font.render(l, 1, (255,255,255), (0,0,0)).get_rect()
+        tr.center = (px, py)
+        scr.blit(font.render(l, 1, (255,255,255), (0,0,0)), tr)
+        py += fontHeight
 
-def pause():
+
+    
+def pause(key=K_ESCAPE):
     paused = True
     while paused:
         for event in pygame.event.get():
@@ -44,7 +58,7 @@ def pause():
                 pygame.quit()
                 sys.exit()
             elif event.type == KEYDOWN:
-                if event.key == K_ESCAPE:
+                if event.key == key:
                     paused = False
 
 def ptc(dist, theta):
@@ -61,13 +75,13 @@ def collision(pdy):
     #y = py -> ya = -1
     #y = py + 140 -> ya = 1
     #print(x, y, xa, ya, a, py, cy, r, pdy, y - pdy)     
-    a = 180 - scl(abs(y - pdy), 0, 140, 90, 270) + 360
-    print(a)
+    a = 180 - scl(abs(y - pdy), 0, 140, 120, 240) + 360
+    # print(a)
 
 def ai():
-    global cy, y, ya
+    global cy, y, ya, pos
     # cy = y
-    sp = abs((y - cy) / 10) 
+    sp = (abs(y - cy) - pos) / 75
     if y < cy:
         if ya < 0:
             cy -= (sp + 0) 
@@ -87,17 +101,23 @@ def physics():
         
     
     if p.colliderect(b): #Ball has collided with player's paddle
+        global pos
         x = 25
+        pos = rn.randint(-20, 160)
+        print(pos)
         collision(py)
         sp = rn.random() * 10 + 7
         #xa = Math.abs(xa)
         # console.log(sp)
+        #a = rn.randint(90, 270)
     
     if (cp.colliderect(b)): #Ball has collided with computer's paddle
         #xa = Math.abs(xa) * -1
-        a = scl(abs(y - cy), 0, 140, 90, 270) 
-        print(a)
-        x = 465
+        # a = scl(abs(y - cy), 0, 140, 90, 270)
+        a = rn.randint(120, 240) 
+        #print(a)
+        print('Actual: ' + str(y - cy))
+        x = 464
 
     elif x > 500:
         a *= -1; ps += 1; d = 2; x = 250; y = 250; cy = 0
@@ -109,8 +129,32 @@ def physics():
         a *= -1; y = 10
     xa = ptc(r,a)[0]
     ya = ptc(r,a)[1] * -1
-    
+#ps = 494 
+scr.fill(0)
+message('''Instructions:\nUse Up/W to move up\nUse Down/S to move down\nH to show this message\nESC to pause\n\nSPACE to start''', 250, 100)
+pygame.display.flip()
+pause(K_SPACE)
 while True:    
+    if ps > 10:
+        scr.fill(0)
+        d = 3
+        ln += 1
+        message('Level ' + str(ln + 1), 250, 300)
+        message('Press Space to start', 250, 330)
+        pygame.display.flip()
+        pause(K_SPACE)
+        level = levels[ln % 6]
+        ps = 0; cs = 0
+    if ln >= 6:
+        scr.fill(0)
+        message('YOU WIN!\nPress Space to play again', 250, 300)
+        pygame.display.flip()
+        pause(K_SPACE)
+        ln = 0
+        ps, cs = 0, 0
+        continue
+        
+
     #print(clock.get_fps())
     a = a % 360
     c = pygame.Color((255,255,255))
@@ -127,13 +171,18 @@ while True:
                 pu = True
                 pd = False
             
+            elif event.key == K_h:
+                scr.fill(0)
+                message('''Instructions:\nUse Up/W to move up\nUse Down/S to move down\nH to show this message\nESC to pause\n\nSPACE to resume''', 250, 100)
+                pygame.display.flip()
+                pause(K_SPACE)
     
             else:
                 pu = False; pd = False
             if event.key == K_ESCAPE:
                 pause()
         elif event.type == MOUSEMOTION:
-            py = pygame.mouse.get_pos()[1]
+            py = pygame.mouse.get_pos()[1] - 70
 
     # if (! np):
     #     if (event.key == UP_ARROW)):
@@ -161,18 +210,20 @@ while True:
     p = pygame.draw.rect(scr, c, (5, py, 10, 140)) #Player's paddle
     b = pygame.draw.ellipse(scr, c, (x, y, 20, 20)) #Ball
     cp = pygame.draw.rect(scr, c, (485, cy, 10, 140)) #computer's paddle
+    x += xa
+    y += ya
+    message('Player score: ' + str(ps) + '    ' + 'Computer score: ' + str(cs), 250, 550)
+    clock.tick(2000)
+    pygame.display.flip()
     if d > 0:
+        print('\n\n')
+        pos = rn.randint(-20, 160)
         sleep(d)
         intel = m.floor(rn.random() * 100)
-        r = (rn.random() * 3) + 0.2
+        r = (rn.random() * level * 2) + (level * 2)        
         a = (rn.random()) + 45 + (90 * m.floor(rn.random() * 4))
         xa = ptc(r,a)[0]
         ya = ptc(r,a)[1]
         pygame.display.flip()
-    
     d = 0
-    x += xa
-    y += ya
-    message('Player score: ' + str(ps) + '    ' + 'Computer score: ' + str(cs), 250, 550)
-    pygame.display.flip()
-    clock.tick(100)
+
